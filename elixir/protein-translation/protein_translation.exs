7682992c9749ee33e6e5083e5b32fa278f8b9a5f
Geordie443 <geordie191@gmail.com>
2018-06-06 15:06:05 -0400

    updated protein translation to do validation on rna and codons

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -4,35 +4,39 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    cond do
+      Regex.run(~r/([^ACGU])/, rna) == nil ->
+        codon_list = for <<x::binary-3 <- rna>>, do: x
 
-    codon_list =
-      for <<x::binary-3 <- rna>>, do: x
+        protein_map_list =
+          Enum.map(codon_list, fn x ->
+            ProteinTranslation.of_codon(x)
+          end)
 
-    protein_map_list = Enum.map(codon_list, fn(x) -> 
-      ProteinTranslation.of_codon(x) 
-    end)
-    
-    size = Enum.count(protein_map_list)
+        size = Enum.count(protein_map_list)
 
-    protein_list = []
+        protein_list = []
 
-    protein_list = for x <- 0..(size-1) do
-      new_protein = ProteinTranslation.protein_return(x ,protein_map_list)
-      protein_list ++ new_protein
+        protein_list =
+          for x <- 0..(size - 1) do
+            new_protein = ProteinTranslation.protein_return(x, protein_map_list)
+            protein_list ++ new_protein
+          end
+
+        protein_string = List.to_string(protein_list)
+
+        new_protein_string =
+          protein_string
+          |> String.replace(~r/(STOP)(.*)()/, "")
+          |> String.split()
+
+        {:ok, new_protein_string}
+
+      Regex.run(~r/([^ACGU])/, rna) != nil ->
+        {:error, "invalid RNA"}
     end
-    
-    protein_string = List.to_string(protein_list)
-
-    new_protein_string = 
-      protein_string
-      |> String.replace(~r/(STOP)(.*)()/, "")
-      |> String.split()
-
-      {:ok, new_protein_string}
-
-
   end
-  
+
   @doc """
   Given a codon, return the corresponding protein
 
@@ -56,11 +60,10 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    # in_codon = codon
+    # the_protein = "Methionine"
 
-    #in_codon = codon
-    #the_protein = "Methionine"
-
-    #translation_map =  %{codon: in_codon, protein: the_protein}
+    # translation_map =  %{codon: in_codon, protein: the_protein}
 
     protein_maps = [
       %{codon: "UGU", protein: "Cysteine"},
@@ -69,7 +72,7 @@ defmodule ProteinTranslation do
       %{codon: "UUG", protein: "Leucine"},
       %{codon: "AUG", protein: "Methionine"},
       %{codon: "UUU", protein: "Phenylalanine"},
-      %{codon: "UUC", protein: "Phenylalanine"}, 
+      %{codon: "UUC", protein: "Phenylalanine"},
       %{codon: "UCU", protein: "Serine"},
       %{codon: "UCC", protein: "Serine"},
       %{codon: "UCA", protein: "Serine"},
@@ -79,31 +82,35 @@ defmodule ProteinTranslation do
       %{codon: "UAC", protein: "Tyrosine"},
       %{codon: "UAA", protein: "STOP"},
       %{codon: "UAG", protein: "STOP"},
-      %{codon: "UGA", protein: "STOP"},]
+      %{codon: "UGA", protein: "STOP"}
+    ]
 
-    #if translation_map.codon == "AUG" do
-        #{:ok, translation_map.protein}
-    #end
+    # if translation_map.codon == "AUG" do
+    # {:ok, translation_map.protein}
+    # end
 
-    returned_map = ProteinTranslation.map_match(protein_maps, codon)
-    returned_protein = returned_map.protein
-    {:ok, returned_protein}
- 
+    cond do
+      Regex.run(~r/([^ACGU])/, codon) == nil ->
+        returned_map = ProteinTranslation.map_match(protein_maps, codon)
+        returned_protein = returned_map.protein
+        {:ok, returned_protein}
+
+      Regex.run(~r/([^ACGU])/, codon) != nil ->
+        {:error, "invalid codon"}
+    end
   end
-
 
   def map_match(array, codon) do
     test_map = %{codon: codon, protein: ""}
-    Enum.find(array, fn(protein_map) ->
-      test_map.codon == protein_map.codon 
-    end) 
+
+    Enum.find(array, fn protein_map ->
+      test_map.codon == protein_map.codon
+    end)
   end
 
   def protein_return(index, array) do
-      {:ok, protein} = Enum.at(array, index)
-      string = "#{protein} "
-      string
+    {:ok, protein} = Enum.at(array, index)
+    string = "#{protein} "
+    string
   end
-
-
 end
